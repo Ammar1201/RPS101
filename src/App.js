0d1554f78a8b84b310e519c6a41fb634a07a7509
Ammar1201 <aamour1201@gmail.com>
2022-11-25 16:20:09 +0200

    Spinner style updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
@@ -6,18 +6,22 @@ import About from "./pages/About";
 import Wiki from "./pages/Wiki";
 import Play from "./pages/Play";
 import ScoreBoard from "./pages/ScoreBoard";
+import Spinner from "./components/utils/Spinner";
+import PlayerVsPlayer from "./pages/PlayerVsPlayer";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <Fragment>
       <Header />
+      {isLoading && <Spinner />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/wiki" element={<Wiki />} />
         <Route path="/play" element={<Play />} />
         <Route path="/scoreboard" element={<ScoreBoard />} />
-        <Route path="/play-vs-player" element={<Play />} />
+        <Route path="/play-vs-player" element={<PlayerVsPlayer setIsLoading={setIsLoading} />} />
         <Route path="/play-vs-ai" element={<Play />} />
       </Routes>
     </Fragment>
--- src/components/utils/Spinner.js
+++ src/components/utils/Spinner.js
@@ -2,8 +2,11 @@ import classes from './Spinner.module.css';
 
 const Spinner = () => {
   return ( 
-    <div className={classes.loaderContainer}>
-      <div className={classes.loader}></div> 
+    // <div className={classes.loaderContainer}>
+    //   <div className={classes.loader}></div> 
+    // </div>
+    <div id={'loader'} className={classes.loading}>
+      <span className={classes.loader}></span> 
     </div>
   );
 }
