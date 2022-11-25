4079be50ea655765afa1afd5adfe016dba0063b7
Ammar1201 <aamour1201@gmail.com>
2022-11-25 13:40:08 +0200

    added basic Home page layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
+import { Fragment } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import About from "./pages/About";
 import Wiki from "./pages/Wiki";
+import Play from "./pages/Play";
+import ScoreBoard from "./pages/ScoreBoard";
 
 const App = () => {
   return (
-    <>
+    <Fragment>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/wiki" element={<Wiki />} />
+        <Route path="/play" element={<Play />} />
+        <Route path="/scoreboard" element={<ScoreBoard />} />
       </Routes>
-    </>
+    </Fragment>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,26 @@
+import classes from './Home.module.css';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
   return ( 
     <div>
-      Home
+      <div className={classes.container}>
+        <div className={classes.header}>
+          <div className={classes.title}>
+            <h1>
+              Welcome To RPS - 101 
+            </h1>
+            <h2>
+              RPS = Rock, Papers, Scissors With 101 Objects
+            </h2>
+          </div>
+        </div>
+        <div className={classes.content}>
+            <Link to='/play' >Play</Link>
+            <Link to='/scoreboard' >Score Board</Link>
+            <Link to='/wiki' >Wiki</Link>
+        </div>
+      </div>
     </div> 
   );
 }
